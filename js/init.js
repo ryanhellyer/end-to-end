7884a05196509343dbc25936425315ece2c1489f
ryanhellyer <ryanhellyer@gmail.com>
2014-10-31 13:03:49 +0100

    Finishing decrypt button functionality

--- js/init.js
+++ js/init.js
@@ -1,11 +1,17 @@
 // Implementing End to end encryption within WordPress
 jQuery(function($){
 
+	if (typeof end2end_set != 'undefined') {
+		var end2end_decrypt_button = '<span id="end2end-submit" class="preview button" id="end2end-submit">Decrypt</span>';
+	} else {
+		var end2end_decrypt_button = '';
+	}
+
 	// JS for the backend
 	if (typeof end2end_frontend == 'undefined') {
 
 		// Inject the form into the page
-		var end2end_form = '<label>'+end2end_label+'</label> <input type="password" id="end2end-key" name="end2end-key" /><input id="end2end-submit" class="preview button" type="submit" id="end2end-submit" value="Decrypt" /><div style="display:none" id="end2end-temporary-storage"></div>';
+		var end2end_form = '<label>'+end2end_label+'</label> <input type="password" id="end2end-key" name="end2end-key" />'+end2end_decrypt_button+'<div style="display:none" id="end2end-temporary-storage"></div>';
 		$("#end2end-backend-form").html(end2end_form);
 
 		// Set temporary storage area
@@ -16,10 +22,7 @@ jQuery(function($){
 		if (typeof end2end_set != 'undefined') {
 			if (true == end2end_set) {
 				// Decrypt from temporary storage
-
-end2end-submit
-z
-				$('#end2end-key').onclick(     keyup(function() {
+				$('#end2end-submit').click(function() {
 					var end2end_key = $('#end2end-key').val();
 					var end2end_text = $('#end2end-temporary-storage').html();
 					var end2end_blob = Aes.Ctr.decrypt(end2end_text, end2end_key, 256)
